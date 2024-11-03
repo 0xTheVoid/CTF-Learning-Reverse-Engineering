dab7dd87a3e0c6f5e7b4fa969341966c3ecb7413
hack-the-void <joshua.intruder@gmail.com>
2024-11-03 15:22:59 +0100

    Update LastWords.c

--- Challenge3/LastWords.c
+++ Challenge3/LastWords.c
@@ -27,8 +27,8 @@ void display_banner() {
 
 // Function to check the activation code
 int check_code(char *input) {
-    // Correct password "VOID{HIDDEN}" obfuscated with XOR 0x42
-    char correct_code[] = {0x14, 0xd, 0xb, 0x6, 0x39, 0xa, 0x73, 0x6, 0x6, 0x71, 0xc, 0x1d, 0x1, 0x72, 0x6, 0x71, 0x3f};
+    // Correct password "VOID{H1DD3N_C0D3}" obfuscated with XOR 0x42
+    char correct_code[] = {0x14, 0xd, 0xb, 0x6, 0x39, 0xa, 0x73, 0x6, 0x6, 0x71, 0xc, 0x1d, 0x1, 0x72, 0x6, 0x71, 0x3f, 0x00};
 
     for (int i = 0; correct_code[i] != '\0'; i++) {
         if (input[i] != (correct_code[i] ^ 0x42)) { // Compare input with de-obfuscated code
@@ -39,12 +39,12 @@ int check_code(char *input) {
 }
 
 int main() {
-    char input[15] = {0}; // Initialize to 0 to ensure null-termination
+    char input[18] = {0}; // Initialize to 0 to ensure null-termination
 
     display_banner(); // Display the banner and the lore
 
     printf("Enter the activation code: ");
-    scanf("%14s", input); // Limit to 14 characters to prevent overflow
+    scanf("%17s", input); // Limit to 14 characters to prevent overflow
 
     // Check if the activation code is correct
     if (check_code(input)) {
